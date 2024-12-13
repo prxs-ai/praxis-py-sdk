1ba9773cb1959c4ee21af41164e69836f398e82b
hexavor <psinitsa97@gmail.com>
2024-12-13 14:48:15 +0300

    twitter

--- praxis-sdk-core-clean/src/shared/utils.py
+++ praxis-sdk-core-clean/src/shared/utils.py
@@ -1,6 +1,7 @@
 import logging
 import json
-from services.ai_tools.openai_client import send_openai_request
+# from services.ai_tools.openai_client import send_openai_request OPENAI
+from services.ai_tools.claude_client_instance import client
 
 
 async def format_text(text: str) -> str:
@@ -21,7 +22,13 @@ async def format_text(text: str) -> str:
             }
         ]
 
-        text = await send_openai_request(messages=messages)
+        system_prompt = (
+        "You are a text shortener. Your task is to reduce the text length, keeping "
+        "its meaning and style unchanged. You can remove some sentences as long as it "
+        "doesn't harm the overall meaning of the text. Also remove emojis. DO NOT REMOVE HASHTAGS\n\n"
+        f"{text}"
+)
+        text = await client.generate_response(prompt=system_prompt, temperature=1.1)
         logging.info(f'Tweet validating 1 {text}')
         text = await add_blank_lines(text)
 
@@ -54,6 +61,20 @@ Who needs thumbs? Unleash the hyper-advanced AI bot and watch it fetch not just 
             "content": text
         }
     ]
-    text = await send_openai_request(messages=messages)
+
+    formatter_prompt = (
+            "You are a text formatter. Your task is only to format the text. \n"
+            "The text should be split into several paragraphs with a blank line between them. \n"
+            "Do not change the content of the text, just insert blank lines to divide it into paragraphs.\n\n"
+            "EXAMPLE INPUT:\n"
+            "Discover $NFNT, where sci-fi meets reality! With NFINITY, even your dog's to-do list becomes autonomous. Who needs thumbs? Unleash the hyper-advanced AI bot and watch it fetch not just sticks but ROI. 🐶🔥 #NFINITY \n\n"
+            "EXAMPLE OUTPUT:\n"
+            "Discover $NFNT, where sci-fi meets reality!\n\n"
+            "With NFINITY, even your dog's to-do list becomes autonomous.\n\n"
+            "Who needs thumbs? Unleash the hyper-advanced AI bot and watch it fetch not just sticks but ROI. 🐶🔥 \n\n"
+            "#NFINITY @nfinityAI 🚀\n\n"
+            f"{text}"
+            )
+    text = await client.generate_response(prompt=formatter_prompt)
     logging.info(f'Tweet validating 2 {text}')
     return text
