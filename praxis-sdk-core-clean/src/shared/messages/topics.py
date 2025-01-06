162977f5d6f1ac0c157c5ea3c8ec91d47da3a421
0xDevZip <0xdevzip@mail.ru>
2025-01-06 10:05:55 +0100

    feat: included additional topics

--- praxis-sdk-core-clean/src/shared/messages/topics.py
+++ praxis-sdk-core-clean/src/shared/messages/topics.py
@@ -3,4 +3,7 @@ from typing import Final
 
 
 class Topics(StrEnum):
-    SOCIAL: Final[str] = "SOCIAL"
+    SOCIAL: Final[str] = "SOCIAL_GENERIC"
+
+    TELEGRAM: Final[str] = "TELEGRAM"
+    TWITTER: Final[str] = "TWITTER"
