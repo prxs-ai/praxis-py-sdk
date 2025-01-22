a57419a5e522a1c9d8b34585210f94e9ead75eaa
hyp0cr4t <beaumont121990@gmail.com>
2025-01-22 21:55:38 +0000

    replace postgres_dsn

--- praxis-sdk-core-clean/src/infra/configs/config.py
+++ praxis-sdk-core-clean/src/infra/configs/config.py
@@ -41,17 +41,18 @@ class InfrastructureConfig(BaseSettings):
     def qdrant_url(self) -> str:
         return f"http://{self.QDRANT_HOST}:{self.QDRANT_PORT}"
 
-    # @field_validator('postgres_dsn', mode='before')
-    # @classmethod
-    # def get_postgres_dsn(cls, _, info: ValidationInfo):
-    #     return PostgresDsn.build(
-    #         scheme='postgresql+asyncpg',
-    #         username=info.data['postgres_user'],
-    #         password=info.data['postgres_password'],
-    #         host=info.data['postgres_host'],
-    #         port=info.data['postgres_port'],
-    #         path=info.data['postgres_db'],
-    #     )
+    @field_validator('postgres_dsn', mode='before')
+    @classmethod
+    def get_postgres_dsn(cls, _, info: ValidationInfo):
+        return f"postgresql+asyncpg://{info.data['postgres_user']}:{info.data['postgres_password']}@{info.data['postgres_host']}:{info.data['postgres_port']}/{info.data['postgres_db']}"
+        return PostgresDsn.build(
+            scheme='postgresql+asyncpg',
+            username=info.data['postgres_user'],
+            password=info.data['postgres_password'],
+            host=info.data['postgres_host'],
+            port=info.data['postgres_port'],
+            path=info.data['postgres_db'],
+        )
 
     @field_validator('redis_dsn', mode='before')
     @classmethod
