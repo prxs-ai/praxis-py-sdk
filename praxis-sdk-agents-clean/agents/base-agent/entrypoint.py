d584492b011034b3ae51ef5774c21cb0f1a9d0fc
hyp0cr4t <beaumont121990@gmail.com>
2025-02-20 21:12:17 +0000

    add base agent

--- praxis-sdk-agents-clean/agents/base-agent/entrypoint.py
+++ praxis-sdk-agents-clean/agents/base-agent/entrypoint.py
@@ -1,6 +1,22 @@
 from agent.config import get_agent_config
-from agent.utils import get_entry_point
+from agent.utils import get_entry_points
 
 config = get_agent_config()
 
-app = get_entry_point(config.group_name, config.entrypoint_name).load()
+
+def get_agent():
+    entrypoints = get_entry_points(config.group_name)
+    try:
+        print(f"Loading {config.target_entrypoint} entrypoint...")
+        return entrypoints[config.target_entrypoint].load()
+    except KeyError:
+        print(f"Entrypoint {config.target_entrypoint} not found. Loading {config.default_entrypoint} entrypoint...")
+        return entrypoints[config.default_entrypoint].load()
+
+app = get_agent()
+
+
+if __name__ == "__main__":
+    from ray import serve
+
+    serve.run(app, route_prefix='/')
--- praxis-sdk-agents-clean/agents/base-agent/src/agent/ray_entrypoint.py
+++ praxis-sdk-agents-clean/agents/base-agent/src/agent/ray_entrypoint.py
@@ -1,30 +1,21 @@
 from contextlib import asynccontextmanager
+from typing import Any
 from urllib.parse import urljoin
 
 import requests
 from fastapi import FastAPI
-from ray import serve
 
 
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    # launch some tasks on app start
-    yield
-    # handle clean up
-
-app = FastAPI(lifespan=lifespan)
-
-
-@serve.deployment
-@serve.ingress(app)
 class BaseAgent:
     def __init__(self, *args, **kwargs):
         pass
 
-    @app.post("/{goal}")
     def handle(self, goal: str, plan: dict | None = None):
         """This is one of the most important endpoint of MAS.
         It handles all requests made by handoff from other agents or by user."""
+        pass
+
+    def reconfigure(self, config: dict[str, Any]):
         pass
 
     def handoff(self, endpoint: str, goal: str, plan: dict):
@@ -32,4 +23,23 @@ class BaseAgent:
         and decide to handoff the task to another agent. """
         return requests.post(urljoin(endpoint, goal), json=plan).json()
 
-app = BaseAgent.bind()
+
+def agent_builder(args: dict):
+    from ray import serve
+
+    @asynccontextmanager
+    async def lifespan(app: FastAPI):
+        # launch some tasks on app start
+        yield
+        # handle clean up
+
+    app = FastAPI(lifespan=lifespan)
+
+    @serve.deployment
+    @serve.ingress(app)
+    class Agent(BaseAgent):
+        @app.post("/{goal}")
+        async def handle(self, goal: str, plan: dict | None = None):
+            return super().handle(goal, plan)
+
+    return Agent.bind(**args)
