3c17edbadebb5865f04f0e5a979ce4b5453551b2
hyp0cr4t <beaumont121990@gmail.com>
2025-02-21 07:02:24 +0000

    add some utils

--- praxis-sdk-agents-clean/agents/base-agent/src/agent/ray_entrypoint.py
+++ praxis-sdk-agents-clean/agents/base-agent/src/agent/ray_entrypoint.py
@@ -2,8 +2,12 @@ from contextlib import asynccontextmanager
 from typing import Any
 from urllib.parse import urljoin
 
+import ray
 import requests
 from fastapi import FastAPI
+from ray import workflow
+
+from agent.utils import generate_request_id
 
 
 class BaseAgent:
@@ -13,7 +17,51 @@ class BaseAgent:
     def handle(self, goal: str, plan: dict | None = None):
         """This is one of the most important endpoint of MAS.
         It handles all requests made by handoff from other agents or by user."""
-        pass
+        request_id = generate_request_id.bind()
+
+
+        return self._run_workflow(request_id, goal)
+
+    def get_most_relevant_agents(self, goal: str):
+        """This method is used to find the most useful agents for the given goal."""
+        return []
+
+    def get_most_relevant_tools(self, goal: str):
+        """This method is used to find the most useful tools for the given goal."""
+        return [{"name": "example-tool", "version": "0.0.5", "openai_function_spec": {
+            "type": "function", "function": {
+                "name": "example_tool",
+                "description": "A tool that returns the string passed in as an output.",
+                "parameters": {
+                    "type": "object",
+                    "properties": {
+                        "message": {
+                            "type": "string",
+                            "description": "The message to echo.",
+                            "default": "Hello World"
+                        }
+                    },
+                    "required": ["message"]
+                },
+                "output": { "type": "object", "properties": {
+                    "result": { "type": "string", "description": "Returns the input string as output." }
+                }}
+            }
+        }}]
+
+    def generate_plan(self, goal: str):
+        """This method is used to generate a plan for the given goal."""
+        return {"steps": []}
+
+    @ray.remote
+    def _run_workflow(self, request_id: str, goal: str):
+
+        agents = self.get_most_relevant_agents(goal)
+        tools = self.get_most_relevant_tools(goal)
+
+
+        return workflow.continuation(request_id)
+
 
     def reconfigure(self, config: dict[str, Any]):
         pass
