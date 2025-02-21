a8c2904bb32a60346a11c9f21c7d63841ed264ef
hyp0cr4t <beaumont121990@gmail.com>
2025-02-21 07:02:24 +0000

    add some utils

--- praxis-sdk-agents-clean/agents/base-agent/src/agent/utils.py
+++ praxis-sdk-agents-clean/agents/base-agent/src/agent/utils.py
@@ -1,6 +1,9 @@
 
 import sys
+import uuid
 from importlib.metadata import EntryPoint, entry_points
+
+import ray
 
 
 def get_entry_points(group: str) -> list[EntryPoint]:
@@ -12,3 +15,10 @@ def get_entry_points(group: str) -> list[EntryPoint]:
         return entrypoints.get(group, [])
     except AttributeError:
         return entrypoints.select(group=group)
+
+
+
+@ray.remote
+def generate_request_id() -> str:
+   # Generate a unique idempotency token.
+   return uuid.uuid4().hex
