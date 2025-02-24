f32e85df7a81fe1a6b18905e0233815456474fb0
hyp0cr4t <beaumont121990@gmail.com>
2025-02-24 21:26:24 +0000

    add base agent

--- praxis-sdk-agents-clean/agents/base-agent/entrypoint.py
+++ praxis-sdk-agents-clean/agents/base-agent/entrypoint.py
@@ -1,24 +1,6 @@
-from base_agent.config import get_agent_config
-from base_agent.utils import get_entrypoint
+from agent.config import get_agent_config
+from agent.utils import get_entry_point
 
-app = get_entrypoint(get_agent_config().group_name).load()
+config = get_agent_config()
 
-
-if __name__ == "__main__":
-    import uvicorn
-    from fastapi import FastAPI
-    from ray import serve
-
-    # Run Ray Serve in local testing mode
-    handle = serve.run(app(get_agent_config().model_dump()),
-                      route_prefix='/',
-                      _local_testing_mode=True)
-
-    fastapi_app = FastAPI()
-
-    @fastapi_app.post("/{goal}")
-    async def handle_request(goal: str):
-        return await handle.handle.remote(goal)
-
-    # Run uvicorn server
-    uvicorn.run(fastapi_app, host="0.0.0.0", port=8000)
+app = get_entry_point(config.group_name, config.entrypoint_name).load()
