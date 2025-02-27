592284331009465ebec1077f33f0c8b17cd3a4f7
hyp0cr4t <beaumont121990@gmail.com>
2025-02-28 00:35:00 +0300

    upd env vars

--- praxis-sdk-agents-clean/agents/base-agent/src/base_agent/utils.py
+++ praxis-sdk-agents-clean/agents/base-agent/src/base_agent/utils.py
@@ -5,8 +5,8 @@ from importlib.metadata import EntryPoint, entry_points
 def get_entry_points(group: str) -> list[EntryPoint]:
     if sys.version_info >= (3, 10):
         entrypoints = entry_points(group=group)
-
-    entrypoints = entry_points()
+    else:
+        entrypoints = entry_points()
     try:
         return entrypoints.get(group, [])
     except AttributeError:
@@ -16,9 +16,9 @@ def get_entry_points(group: str) -> list[EntryPoint]:
 def get_entrypoint(group_name: str, target_entrypoint: str = "target", default_entrypoint: str = "basic") -> EntryPoint:
     entrypoints = get_entry_points(group_name)
     try:
-        return entrypoints[target_entrypoint]
+        return entrypoints.select(name=target_entrypoint)[0]
     except KeyError:
-        return entrypoints[default_entrypoint]
+        return entrypoints.select(name=default_entrypoint)[0]
 
 
 
