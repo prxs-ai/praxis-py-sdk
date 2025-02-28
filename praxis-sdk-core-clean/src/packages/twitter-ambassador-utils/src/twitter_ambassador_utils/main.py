92acc8516ed06f49976414331abf685f09d02633
0xDevZip <0xdevzip@mail.ru>
2025-03-01 02:49:02 +0300

    [PRX-82] :: Edit packages

--- praxis-sdk-core-clean/src/packages/twitter-ambassador-utils/src/twitter_ambassador_utils/main.py
+++ praxis-sdk-core-clean/src/packages/twitter-ambassador-utils/src/twitter_ambassador_utils/main.py
@@ -1,6 +1,22 @@
 import aiohttp
 
 
+async def post_request(url: str, token: str, payload: dict):
+    headers = {
+        "Authorization": f"Bearer {token}",
+        "Content-Type": "application/json"
+    }
+
+    async with aiohttp.ClientSession() as session:
+        async with session.post(url, headers=headers, json=payload) as response:
+            if response.ok:
+                data = await response.json()
+                print(f"Request successful: {data}")
+                return data
+            print(f"Request failed. Status: {response.status}, Response: {await response.text()}")
+            return await response.json()
+
+
 async def create_post(
     access_token: str,
     tweet_text: str,
@@ -29,3 +45,15 @@ async def create_post(
                 return result
             else:
                 print(f'Twit not posted: {await response.text()}')
+
+
+async def retweet(token: str, user_id: str, tweet_id: str):
+    url = f"https://api.x.com/2/users/{user_id}/retweets"
+    payload = {"tweet_id": tweet_id}
+    return await post_request(url, token, payload)
+
+
+async def set_like(token: str, user_id: str, tweet_id: str):
+    url = f"https://api.x.com/2/users/{user_id}/likes"
+    payload = {"tweet_id": tweet_id}
+    return await post_request(url, token, payload)
