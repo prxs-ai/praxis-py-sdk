f525622c04599876a86e710b59039d27fce7b8fe
hyp0cr4t <beaumont121990@gmail.com>
2025-02-28 09:34:05 +0000

    temp fix the prompt

--- praxis-sdk-agents-clean/agents/base-agent/src/base_agent/prompt/__init__.py
+++ praxis-sdk-agents-clean/agents/base-agent/src/base_agent/prompt/__init__.py
@@ -7,7 +7,12 @@ from base_agent.utils import get_entrypoint
 def prompt_builder(*args, **kwargs):
     # get the package name from the entrypoint
     config = get_prompt_config()
-    entrypoint = get_entrypoint(config.group_name)
-    package_name, _ = entrypoint.value.rsplit(":")
+    try:
+        entrypoint = get_entrypoint(config.group_name)
+    except IndexError:
+        # if the entrypoint is not found, use the default package name
+        package_name = "base_agent.prompt"
+    else:
+        package_name, _ = entrypoint.value.rsplit(":")
 
     return PromptBuilder(jinja2_env=get_environment(package_name))
