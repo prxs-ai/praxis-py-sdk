931061df43dd1b2aeafabded959ad9bcd732d9c8
hexavor <psinitsa97@gmail.com>
2025-03-04 12:08:37 +0100

    PRX-85

---
+++ praxis-sdk-core-clean/src/packages/twitter-follow-unfollow-get-likes-on-post/src/twitter_follow_unfollow_get_likes_on_post/main.py
@@ -0,0 +1,24 @@
+import aiohttp
+
+from infrastructure.configs.logger import get_logger
+
+logger = get_logger(__name__)
+
+
+async def get_likes_on_post(access_token: str,tweet_id: str):
+    logger.info("Getting user notifications")
+    url = f"https://api.x.com/2/tweets/{tweet_id}/liking_users"
+
+    headers = {"Authorization": f"Bearer {access_token}"}
+
+    async with (
+        aiohttp.ClientSession() as session,
+        session.get(url, headers=headers) as response,
+    ):
+        print(response)
+        if response.status == 200:
+            result = await response.json()
+            logger.info(f'Notifications received: {result}')
+            return result
+        else:
+            logger.info(f'Notifications not received: {await response.text()}')
