2d80f817f69db2dcfc3d22a6273940ff277385a8
0xDevZip <0xdevzip@mail.ru>
2025-03-07 11:07:48 +0100

    feat: init added

--- praxis-sdk-core-clean/src/shared/clients/tweetscout/__init__.py
+++ praxis-sdk-core-clean/src/shared/clients/tweetscout/__init__.py
@@ -2,9 +2,29 @@ __all__ = (
     "TweetScoutAPI",
     "TweetScoutSession",
     "build_query",
+    "And",
+    "Filter",
+    "FromUser",
+    "Hashtag",
+    "MinFavorites",
+    "MinReplies",
+    "MinRetweets",
+    "Negate",
+    "Word",
 )
 
 
 from .api import TweetScoutAPI
-from .query import build_query
+from .query import (
+    And,
+    Filter,
+    FromUser,
+    Hashtag,
+    MinFavorites,
+    MinReplies,
+    MinRetweets,
+    Negate,
+    Word,
+    build_query,
+)
 from .session import TweetScoutSession
