93b32201135918a1dd85474aa4558cf266c17763
ruthuwjwb <inceinyasam26368+irnq8i@gmail.com>
2025-03-18 18:05:52 +0300

    PRX-80-use-redis-with-mem0

--- praxis-sdk-agents-clean/agents/base-agent/src/base_agent/memory/__init__.py
+++ praxis-sdk-agents-clean/agents/base-agent/src/base_agent/memory/__init__.py
@@ -1,6 +1,8 @@
 from base_agent.const import EntrypointGroup
+from base_agent.prompt.config import BasicPromptConfig
 from base_agent.utils import get_entrypoint
 
 
 def memory_builder(*args, **kwargs):
-    return get_entrypoint(EntrypointGroup.MEMORY_ENTRYPOINT).load()()
+    config: BasicPromptConfig = get_entrypoint(EntrypointGroup.MEMORY_CONFIG_ENTRYPOINT).load()
+    return get_entrypoint(EntrypointGroup.MEMORY_ENTRYPOINT).load()(config())
