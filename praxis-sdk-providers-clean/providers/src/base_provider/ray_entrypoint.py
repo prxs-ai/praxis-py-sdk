b4fb158edd775f57b2115498dcec0f0eed013af7
hyp0cr4t <beaumont121990@gmail.com>
2025-03-21 15:56:56 +0000

    upd

--- praxis-sdk-providers-clean/providers/src/base_provider/ray_entrypoint.py
+++ praxis-sdk-providers-clean/providers/src/base_provider/ray_entrypoint.py
@@ -1,9 +1,3 @@
-from collections.abc import Sequence
-from typing import Any
-from urllib.parse import urljoin
-
-import requests
-
 from base_provider import abc
 from base_provider.bootstrap import bootstrap_main
 from base_provider.config import BasicProviderConfig, get_provider_config
@@ -14,6 +8,7 @@ class BaseProvider(abc.AbstractProvider):
 
     def __init__(self, config: BasicProviderConfig, *args, **kwargs):
         self.config = config
+
 
 def provider_builder(args: dict):
     return bootstrap_main(BaseProvider).bind(config=get_provider_config(**args))
