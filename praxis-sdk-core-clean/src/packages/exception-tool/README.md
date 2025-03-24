4770298182684682533284d5ab0e2f65dc329daf
0xDevZip <0xdevzip@mail.ru>
2025-03-24 20:36:52 +0300

    [PRX-93] :: edit retry strategy

--- praxis-sdk-core-clean/src/packages/exception-tool/README.md
+++
@@ -1,2 +0,0 @@
-## Description
-Package that consists exceptions of Praxis project
--- praxis-sdk-core-clean/src/packages/tool-exception/src/tool-exception/main.py
+++ praxis-sdk-core-clean/src/packages/tool-exception/src/tool-exception/main.py
@@ -1,3 +1,3 @@
-class BaseBusinessError(Exception):
+class BaseToolException(Exception):
     """Base exception for inheritance to add to AllowList in Ray system"""
     pass
