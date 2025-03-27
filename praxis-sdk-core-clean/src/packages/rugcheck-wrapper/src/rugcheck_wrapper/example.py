f5c5e573ea1b544265dc2b3231dba981c0b26304
hexavor <psinitsa97@gmail.com>
2025-03-27 18:10:07 +0100

    PRX-85 (add RugCheck Wrapper )

---
+++ praxis-sdk-core-clean/src/packages/rugcheck-wrapper/src/rugcheck_wrapper/example.py
@@ -0,0 +1,17 @@
+import json
+
+from rugcheck_wrapper.main import RugCheckAPI
+
+
+async def main():
+    api = RugCheckAPI()
+
+    try:
+        data = await api.get_token_report("5fGA1os23NNWzhGYLhrWAEnwKDgUX2RSUNmgJACcY5hb")  # Пример адреса токена
+        print(json.dumps(data, indent=4))
+    finally:
+        await api.close()
+
+if __name__ == "__main__":
+    import asyncio
+    asyncio.run(main())
