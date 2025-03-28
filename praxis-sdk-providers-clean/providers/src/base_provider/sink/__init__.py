037625b506ad31643149b2d7dc95d8f9f870ecd2
hyp0cr4t <beaumont121990@gmail.com>
2025-03-28 09:15:34 +0000

    ref

--- praxis-sdk-providers-clean/providers/src/base_provider/sink/__init__.py
+++ praxis-sdk-providers-clean/providers/src/base_provider/sink/__init__.py
@@ -1,9 +1,13 @@
 from base_provider.abc import AbstractDataSink
 from base_provider.const import EntrypointGroup
-from base_provider.utils import get_entrypoint
+from base_provider.utils import get_entrypoint, get_entrypoints
 
 
-def sink_builder() -> AbstractDataSink:
-    config = get_entrypoint(EntrypointGroup.DATA_SINK_CONFIG_ENTRYPOINT).load()
+def sinks_builder(sinks: list[str]) -> list[AbstractDataSink]:
+    _sinks = []
+    for entrypoint in get_entrypoints(EntrypointGroup.DATA_SINK_CONFIG_ENTRYPOINT):
+        if entrypoint.name in sinks:
+            sink_config = entrypoint.load()()
+            _sinks.append(get_entrypoint(EntrypointGroup.DATA_SINK_ENTRYPOINT, entrypoint.name).load(sink_config))
 
-    return get_entrypoint(EntrypointGroup.DATA_SINK_ENTRYPOINT).load()(config())
+    return _sinks
