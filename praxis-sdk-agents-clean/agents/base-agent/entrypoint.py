4fdf5bca760528a3ea2f305f849043318a87d2e6
ruthuwjwb <inceinyasam26368+irnq8i@gmail.com>
2025-04-03 12:43:16 +0300

    feature/PRX-117-rm-comment

--- praxis-sdk-agents-clean/agents/base-agent/entrypoint.py
+++ praxis-sdk-agents-clean/agents/base-agent/entrypoint.py
@@ -2,7 +2,6 @@ from base_agent.const import EntrypointGroup
 from base_agent.utils import get_entrypoint
 from ray import serve
 
-# Load the bound Serve deployment (e.g. ExampleAgent.bind(config))
 app = get_entrypoint(EntrypointGroup.AGENT_ENTRYPOINT).load()
 
 if __name__ == "__main__":
