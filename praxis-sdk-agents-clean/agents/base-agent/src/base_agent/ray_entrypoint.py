8f5d280b23d3747d983c8fe75101edd8d4cf3c81
hyp0cr4t <beaumont121990@gmail.com>
2025-05-19 14:08:26 +0000

    feature/PRX-171-add-chat-mode

--- praxis-sdk-agents-clean/agents/base-agent/src/base_agent/ray_entrypoint.py
+++ praxis-sdk-agents-clean/agents/base-agent/src/base_agent/ray_entrypoint.py
@@ -3,25 +3,23 @@ from typing import Any
 from urllib.parse import urljoin
 
 import requests
-from ray.serve.deployment import Application
 
-from base_agent import abc
+from base_agent import abc, const
 from base_agent.ai_registry import ai_registry_builder
 from base_agent.bootstrap import bootstrap_main
 from base_agent.config import BasicAgentConfig, get_agent_config
 from base_agent.domain_knowledge import light_rag_builder
-from base_agent.langchain import executor_builder
+from base_agent.langchain import executor, executor_builder
 from base_agent.memory import memory_builder
-from base_agent.models import (
-    AgentModel,
-    GoalModel,
-    InsightModel,
-    MemoryModel,
-    QueryData,
-    ToolModel,
-    Workflow,
-)
+from base_agent.models import AgentModel, GoalModel, InsightModel, MemoryModel, QueryData, Task, ToolModel
 from base_agent.prompt import prompt_builder
+from base_agent.workflows import workflow_builder
+
+
+class BaseAgentInputModel(abc.AbstractAgentInputModel): ...
+
+
+class BaseAgentOutputModel(abc.AbstractAgentOutputModel): ...
 
 
 class BaseAgent(abc.AbstractAgent):
@@ -33,6 +31,7 @@ class BaseAgent(abc.AbstractAgent):
 
     def __init__(self, config: BasicAgentConfig, *args, **kwargs):
         self.config = config
+        self.workflow_runner = workflow_builder()
         self.agent_executor = executor_builder()
         self.prompt_builder = prompt_builder()
 
@@ -49,8 +48,8 @@ class BaseAgent(abc.AbstractAgent):
         self,
         goal: str,
         plan: dict | None = None,
-        context: abc.BaseAgentInputModel | None = None,
-    ) -> abc.BaseAgentOutputModel:
+        context: BaseAgentInputModel | None = None,
+    ) -> BaseAgentOutputModel:
         """This is one of the most important endpoints of MAS.
         It handles all requests made by handoff from other agents or by user.
 
@@ -58,7 +57,7 @@ class BaseAgent(abc.AbstractAgent):
         Otherwise, it follows the standard logic to generate a plan and execute it.
         """
 
-        if plan is not None and plan:
+        if plan is not None:
             result = self.run_workflow(plan, context)
             self.store_interaction(goal, plan, result, context)
             return result
@@ -88,8 +87,8 @@ class BaseAgent(abc.AbstractAgent):
         self,
         goal: str,
         plan: dict,
-        result: abc.BaseAgentOutputModel,
-        context: abc.BaseAgentInputModel | None = None,
+        result: BaseAgentOutputModel,
+        context: BaseAgentInputModel | None = None,
     ) -> None:
         interaction = MemoryModel(
             **{
@@ -208,7 +207,7 @@ class BaseAgent(abc.AbstractAgent):
         past_interactions: Sequence[MemoryModel],
         insights: Sequence[InsightModel],
         plan: dict | None = None,
-    ):
+    ) -> dict[int, Task]:
         """This method is used to generate a plan for the given goal."""
         return self.agent_executor.generate_plan(
             self.prompt_builder.generate_plan_prompt(system_prompt=self.config.system_prompt),
@@ -220,11 +219,51 @@ class BaseAgent(abc.AbstractAgent):
             plan=plan,
         )
 
+    def chat(
+        self,
+        user_prompt: str,
+        action: str | None,
+        context: list[str],
+    ) -> executor.ChatResponse:
+
+        if action == const.Intents.CHANGE_SETTINGS:
+            ...
+
+        if action == const.Intents.ADD_KNOWLEDGE:
+            ...
+
+        if action is None:
+            intent = self.agent_executor.classify_intent(
+                prompt=self.prompt_builder.generate_intent_classifier_prompt(
+                    system_prompt=self.config.system_prompt,
+                    user_prompt=user_prompt,
+                )
+            )
+            if intent == const.Intents.CHANGE_SETTINGS:
+                return executor.ChatResponse(
+                    response_text=const.ExtraQuestions.WHICH_SETTINGS,
+                    action=const.Intents.CHANGE_SETTINGS,
+                )
+            if intent == const.Intents.ADD_KNOWLEDGE:
+                return executor.ChatResponse(
+                    response_text=const.ExtraQuestions.WHAT_INFO,
+                    action=const.Intents.ADD_KNOWLEDGE,
+                )
+        # Final case: the action is const.Intents.CHIT_CHAT or intent is const.Intents.CHIT_CHAT
+        # TODO: transform context (maximum 10 messages into one string)
+        return self.agent_executor.chat(
+            prompt=self.prompt_builder.generate_chat_prompt(
+                system_prompt=self.config.system_prompt,
+                user_prompt=user_prompt,
+                context=context,
+            )
+        )
+
     def run_workflow(
         self,
-        plan: Workflow,
-        context: abc.BaseAgentInputModel | None = None,
-    ) -> abc.BaseAgentOutputModel:
+        plan: dict[int, Task],
+        context: BaseAgentInputModel | None = None,
+    ) -> BaseAgentOutputModel:
         return self.workflow_runner.run(plan, context)
 
     def reconfigure(self, config: dict[str, Any]):
@@ -236,5 +275,8 @@ class BaseAgent(abc.AbstractAgent):
         return requests.post(urljoin(endpoint, goal), json=plan).json()
 
 
-def agent_builder(args: dict) -> Application:
+
+
+
+def agent_builder(args: dict):
     return bootstrap_main(BaseAgent).bind(config=get_agent_config(**args))
