7a22586231db6d943bde99db4770706731e9af3b
hexavor <psinitsa97@gmail.com>
2025-05-21 00:40:23 +0300

    Feature/delete telegramm session (#206)
    * Add .DS_Store to gitignore
    
    * Update branch with latest changes from main
    
    * delete session
    
    ---------

---
+++ praxis-sdk-agents-clean/agents/base-agent/src/base_agent/workflows/config.py
@@ -0,0 +1,12 @@
+from functools import lru_cache
+
+from pydantic_settings import BaseSettings
+
+
+class BasicWorkflowConfig(BaseSettings):
+    WORKFLOW_STEP_MAX_RETRIES: int = 5  # Задаю дефолт так как мне кажется, что она не настолько динамическая
+
+
+@lru_cache
+def get_workflow_config() -> BasicWorkflowConfig:
+    return BasicWorkflowConfig()
