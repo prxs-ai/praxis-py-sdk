019fbfc897c0c458c13096c90588d627609009e3
0xDevZip <0xdevzip@mail.ru>
2025-07-21 15:30:59 +0300

    logger improvemment

--- praxis-sdk-core-clean/packages/agents-tools-logger/src/agents_tools_logger/main.py
+++ praxis-sdk-core-clean/packages/agents-tools-logger/src/agents_tools_logger/main.py
@@ -1,4 +1,5 @@
 import sys
+from typing import Union
 
 from loguru import logger
 
@@ -8,15 +9,31 @@ logger.add(
     sys.stdout,
     format="{time:YYYY-MM-DD HH:mm:ss} | {level} | {file}:{line} - {message}",
     level="INFO",
+    colorize=True
 )
 logger.add(
-    "app.log", rotation="10 MB", compression="zip", level="ERROR"
-)  # Логи в файл, с ротацией
+    "app.log",
+    rotation="10 MB",
+    compression="zip",
+    level="ERROR",
+    backtrace=True,
+    diagnose=True
+)
+
+
+def log_message(message: str, level: str = "INFO", exception: Union[Exception, None] = None) -> None:
+    """Упрощенное логирование с обработкой исключений"""
+    if exception:
+        logger.opt(exception=exception).log(level, message)
+    else:
+        logger.log(level, message)
+
 
 log = logger
+
 if __name__ == "__main__":
-    logger.info("This is an info message.")
+    log_message("This is an info message.")
     try:
         1 / 0
-    except ZeroDivisionError:
-        logger.exception("An error occurred!")
+    except ZeroDivisionError as e:
+        log_message("An error occurred!", level="ERROR", exception=e)
